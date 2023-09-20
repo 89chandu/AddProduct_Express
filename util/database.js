d11fbc6f61335716e3003e448106dc8f073a2181
“Chandu” <Chandubopche321@gmail.com>
2023-09-20 19:44:05 +0530

    Design the User and Product relationship

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node_complete', 'root', '7999Ch@ndu', {
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
   dialect: 'mysql',
   host: 'localhost'
 });
