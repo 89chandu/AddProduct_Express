ee685a19d2b3fb2e67b0f8f48422be72783bdd11
“Chandu” <Chandubopche321@gmail.com>
2023-09-20 19:42:57 +0530

    many relation

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('node_complete', 'root', '7999Ch@ndu', {
   dialect: 'mysql',
   host: 'localhost'
 });
